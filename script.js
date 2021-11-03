dc01b6986db91c782465251381c2d16f6aa64549
Desynq <69401694+Desynq@users.noreply.github.com>
2021-11-03 13:05:27 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
 function myFunction() {
+	let error = [];
 	let input;
 	input = window.prompt('Enter a set of numbers.\nSeparate numbers with a comma\nInsensitive to characters that aren\'t numbers or commas.');
+	input = input.replace(/[^0-9\-.,]/g,'');
+	input = input.split(',');
+	if (input.some(a => a === '') ) {
+		window.alert('Some elements have been left blank!\nYour Input: ' + input);
+		return false;
+	}
+	let dataset = input.map(Number);
+	const n = dataset.length;
+	if (dataset.some(isNaN) ) {
+		for (let i = 0; i < n; i++) {
+			if (isNaN(dataset[i]) ) {
+				error.push(input[i]);
+			}
+		}
+		window.alert('One or more elements are not a valid number!\nErrored Elements: ' + error);
+	}
 	document.getElementById('dataset').innerHTML = input;
+	document.getElementById('n').innerHTML = n;
 }
